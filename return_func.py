01236f734658398256fc2ff772b99a35ebc483dd
Devender0023 <87665445+Devender0023@users.noreply.github.com>
2021-12-30 20:30:54 +0530

    made a function return function.

---
+++ return_func.py
@@ -0,0 +1,11 @@
+from random import choice
+
+def make_laugh_func():
+	def get_laugh():
+		l = choice(('HAHAHAHA', 'lol', 'thehehhe'))
+		return l 
+
+	return get_laugh
+
+laugh = make_laugh_func()
+print(laugh())
