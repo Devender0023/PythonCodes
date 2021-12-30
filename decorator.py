ad98c1f63b1272795498bde2156c44380f7f266d
Devender0023 <87665445+Devender0023@users.noreply.github.com>
2021-12-30 21:30:14 +0530

    Made multiple arguements possible to pass

--- decorator.py
+++ decorator.py
@@ -1,13 +1,23 @@
 def be_polite(fn):
-	def wrapper():
+	def wrapper(*args, **kwargs):
 		print("What a pleasure to meet you!")
-		fn()
+		fn(*args, **kwargs)
 		print("Have a great day!")
 	return wrapper
 
+def shout(fn):
+	def caps(*args, **kwargs):
+		return fn(*args, **kwargs).upper()
+	return caps
+
+@shout
+def order(main, side):
+	return f"Hi, I'd like the {main}, with a side of {side}, please!"
+
 @be_polite # This the syntactic sugar of decorator
-def greet():
-	print("My name is Billu.")
+def greet(name):
+	print(f"My name is {name}.")
 
-
-greet()
+person = input("Enter your name: ")
+greet(person)
+print(order("burger", "fries"))
