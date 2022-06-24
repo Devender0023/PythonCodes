7d6016b97e9e13ce145f6719ca9dd2c7545c83e7
Devender0023 <kumar.devender0023@gmail.com>
2022-06-24 13:39:50 +1000

    Wrote some doctest

---
+++ testing_doc.py
@@ -0,0 +1,61 @@
+def add(a,b):
+	"""
+	>>> add(2,3)
+	6
+	>>> add(100,200)
+	20000
+	"""
+
+	return a * b
+
+
+def return_day(num):
+	"""
+	>>> return_day(1)
+	'Sunday'
+	>>> return_day(8)
+	
+	"""
+
+	days=[
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+        "Saturday"
+	]
+
+	
+	if num>0 and num<=len(days):
+		return days[num-1]
+	return None
+
+
+def last_element(nums):
+	"""
+	>>> last_element([1, 2, 3])
+	3
+	>>> last_element([])
+	
+	"""
+
+	if len(nums) == 0:
+		return None
+	return nums[-1]
+
+
+def number_compare(a, b):
+	"""
+	>>> number_compare(2, 3)
+	'Second is greater'
+	>>> number_compare(2, 2)
+	'Numbers are equal'
+	"""
+
+	if a==b:
+		return "Numbers are equal"
+	elif a>b:
+		return "First is greater"
+	return "Second is greater"
