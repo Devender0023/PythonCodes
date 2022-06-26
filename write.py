b39b683f50bb3e4931fda5c1a65d0887b6beabc3
Devender0023 <kumar.devender0023@gmail.com>
2022-06-26 18:26:28 +1000

    File read

--- write.py
+++ write.py
@@ -1,2 +1,8 @@
-with open("New.txt", "w") as file:
-	file.write("haha\n" * 1000000)
+
+with open("alice.txt") as f:
+	content = f.readlines()
+	dict1 = {'lines':len(content),
+    		'words':sum(len(line.split()) for line in content),
+    		'characters':sum(len(line) for line in content)
+    }
+	print(dict1)
