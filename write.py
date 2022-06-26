d69f8b7cd97ff0858a210c46ab337ccd085302d6
Devender0023 <kumar.devender0023@gmail.com>
2022-06-26 18:46:26 +1000

    find and replace in txt file.

--- write.py
+++ write.py
@@ -1,8 +1,10 @@
 
 with open("alice.txt") as f:
-	content = f.readlines()
-	dict1 = {'lines':len(content),
-    		'words':sum(len(line.split()) for line in content),
-    		'characters':sum(len(line) for line in content)
-    }
-	print(dict1)
+	lines = f.read()
+
+with open("alice.txt", "w") as f1:
+	f1.write(lines.replace("Alice", "Colt"))	
+ #    		'words':sum(len(line.split()) for line in content),
+ #    		'characters':sum(len(line) for line in content)
+ #    }
+	# print(dict1)
