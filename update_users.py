03b1b98c00db7393592f9735a2497ff1956698fd
Devender0023 <kumar.devender0023@gmail.com>
2022-07-03 19:57:42 +1000

    Data Scraping

--- update_users.py
+++ update_users.py
@@ -18,7 +18,27 @@ def update_users(old_f_name, old_l_name, new_f_name, new_l_name):
 
     return f'Users updated: {count}'
 
-print(update_users("Colt", "Steele", "Dev", "Kumar"))
+
+def delete_users(f_name, l_name):
+    with open('users.csv') as file:
+        data = csv.reader(file)
+        users = list(data)
+
+    count = 0
+    with open('users.csv' , 'w') as file:
+        writer = csv.writer(file)
+
+        for user in users:
+            if user[0] == f_name and user[1] == l_name:
+                count+=1
+            else:
+                writer.writerow(user)
+
+    return f'Users deleted: {count}'
+
+
+# print(delete_users('Colt', 'Steele'))
+print(delete_users('Dev', 'Kumar'))
 
 
 
