6a4af03edcf3b72eeb3053a1a59d088040fc70c4
Devender0023 <kumar.devender0023@gmail.com>
2022-07-05 16:05:18 +1000

    Making code better by seperating crawler and game file.

--- Scraper.py
+++ Scraper.py
@@ -1,43 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
+
 from random import choice
 
 
-def scraping():
-    print("Here")
-    quotes_data = []
-    HaveNext = True
-    count = 1
-    # print(soup.contents)
-    while(HaveNext):
-        url = "http://quotes.toscrape.com"
-        new_url = f'{url}/page/{count}'
-        response = requests.get(new_url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        quotes = soup.find_all("div", class_="quote")
-            # print(quotes)
-            # HaveNext = False
 
-        for quote in quotes:
-            text = quote.find("span", class_="text").get_text()
-            author = quote.find("small").get_text()
-            # print(author)
-            link = quote.find("a")["href"]
-            new_link = f'{url}{link}'
-            new_response = requests.get(new_link)
-            new_soup = BeautifulSoup(new_response.text, "html.parser")
-            # print(new_soup.contents)
-            BirthDate = new_soup.find("span", class_="author-born-date").get_text()
-            BirthPlace = new_soup.find("span", class_="author-born-location").get_text()
-            author_list = [text, author, BirthDate, BirthPlace]
-            # print(author_list)
-            quotes_data.append(author_list)
-        if soup.find("li",class_="next") is None:
-            HaveNext = False
-        count+=1
-            # time.sleep(3)
-    print("Scraping finished.")
-    return quotes_data
 
 def game(quotes):
     quote = choice(quotes)
