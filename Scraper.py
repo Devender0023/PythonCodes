1e5b1a564225d6f01db6776dd42e635016753135
Devender0023 <kumar.devender0023@gmail.com>
2022-07-05 21:25:02 +1000

    Added more functionality for a better experience.

--- Scraper.py
+++ Scraper.py
@@ -4,28 +4,39 @@ import csv
 
 
 def game(quotes):
-    print("again")
     quote = choice(quotes)
-    count = 3
+    count = 4
     while(count >= 1):
-        if count == 3:
+        if count == 4:
             print(f'Gusses left {count}')
             print(f"Who is the author of this quote: {quote[0]}")
             answer = input("Make a guess about the author of this quote: ")
-            if answer == quote[1]:
+            if answer.lower() == quote[1].lower():
                 print("Great! You did it.")
                 count -= 1
                 return menu(quotes)
             else:
                 count-=1
                 continue
-        elif count == 2:
+        elif count == 3:
             print(f'Gusses left {count}')
             print(f'Here is a hint for you, the author was born on {quote[2]}.')
             answer = input("Make a guess about the author of this quote: ")
-            if answer == quote[1]:
+            if answer.lower() == quote[1].lower():
                 print("Great! You did it.")
                 count -=1
+                return menu(quotes)
+            else:
+                count-=1
+                continue
+        elif count == 2:
+            print(f'Gusses left {count}')
+            initials = quote[1].split()
+            print(f'Here is a hint for you, the author\'s initial is {initials[0][0]}. {initials[1][0]}.')
+            answer = input("Make a guess about the author of this quote: ")
+            if answer.lower() == quote[1].lower():
+                print("Great! You did it.")
+                count -= 1
                 return menu(quotes)
             else:
                 count-=1
@@ -34,12 +45,13 @@ def game(quotes):
             print(f'Gusses left {count}')
             print(f'Here is another hint for you, the author was born {quote[3]}.')
             answer = input("Make a guess about the author of this quote: ")
-            if answer == quote[1]:
+            if answer.lower() == quote[1].lower():
                 print("Great! You did it.")
                 count -= 1
                 return menu(quotes)
             else:
                 print("Better Luck next time..")
+                print(f'The correct answer is {quote[1]}')
                 count-=1
                 return menu(quotes)
 
@@ -56,7 +68,6 @@ def menu(quotes):
     choice = input("Would you like to play again (yes/no): ")
     while (choice.lower() not in ("no","n")):
         if choice.lower() in ("yes","y"):
-            print("Yes after No")
             return game(quotes)
         else:
             print("Wrong input..! Please try again!")
