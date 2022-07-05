6ad2b9b8248ccdedd4ce3ba45b8a32cbe40f73b7
Devender0023 <kumar.devender0023@gmail.com>
2022-07-05 15:49:45 +1000

    Added game function and a menu for playing again till the user exits.

--- Scraper.py
+++ Scraper.py
@@ -4,6 +4,7 @@ from random import choice
 
 
 def scraping():
+    print("Here")
     quotes_data = []
     HaveNext = True
     count = 1
@@ -20,7 +21,7 @@ def scraping():
         for quote in quotes:
             text = quote.find("span", class_="text").get_text()
             author = quote.find("small").get_text()
-            print(author)
+            # print(author)
             link = quote.find("a")["href"]
             new_link = f'{url}{link}'
             new_response = requests.get(new_link)
@@ -35,12 +36,68 @@ def scraping():
             HaveNext = False
         count+=1
             # time.sleep(3)
-
+    print("Scraping finished.")
     return quotes_data
 
 def game(quotes):
-    while()
     quote = choice(quotes)
-    print("Let's play a game!")
+    count = 3
+    while(count > 0):
+        if count == 3:
+            print(f'Gusses left {count}')
+            print(f"Who is the author of this quote: {quote[0]}")
+            answer = input("Make a guess about the author of this quote: ")
+            if answer == quote[1]:
+                print("Great! You did it.")
+                count -= 1
+                menu(quotes)
+            else:
+                count-=1
+                continue
+        elif count == 2:
+            print(f'Gusses left {count}')
+            print(f'Here is a hint for you, the author was born on {quote[2]}.')
+            answer = input("Make a guess about the author of this quote: ")
+            if answer == quote[1]:
+                print("Great! You did it.")
+                count -=1
+                menu(quotes)
+            else:
+                count-=1
+                continue
+        elif count == 1:
+            print(f'Gusses left {count}')
+            print(f'Here is another hint for you, the author was born {quote[3]}.')
+            answer = input("Make a guess about the author of this quote: ")
+            if answer == quote[1]:
+                print("Great! You did it.")
+                count -= 1
+                menu(quotes)
+            else:
+                print("Better Luck next time..")
+                count-=1
+                break
+
+    menu(quotes)
 
 
+
+def menu(quotes):
+    choice = input("Would you like to play again (y/n): ")
+    while (choice != 'n'):
+        if choice == 'y':
+            game(quotes)
+        else:
+            print("Wrong input..! Please try again!")
+
+    print("Have a nice day!")
+
+
+
+
+
+
+quotes = scraping()
+print("Let's play a guess game!")
+print("Game starts here..")
+game(quotes)
