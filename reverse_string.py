102922c57c9ea03c602f8f212e137106da8cd71f
Devender0023 <kumar.devender0023@gmail.com>
2022-07-08 22:07:51 +1000

    sum pairs code in python list

--- reverse_string.py
+++ reverse_string.py
@@ -16,3 +16,29 @@ def list_check(my_list):
 
 # print(list_check([[],[1],[2,3]]))
 
+def remove_every_other(other_list):
+    new_list = []
+    for count,value in enumerate(other_list):
+        if count%2 == 0:
+            new_list.append(value)
+
+
+    return new_list
+
+# print(remove_every_other([5,1,2,4,1]))
+
+def sum_pairs(nums, num):
+    new_nums = []
+    for i in range(0, len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == num:
+                new_nums.append(nums[i])
+                new_nums.append(nums[j])
+        if len(new_nums) == 2:
+            break
+    return new_nums
+
+
+
+
+print(sum_pairs([4, 2, 10, 5, 1], 15))
