d4d8856beeb447c0317c9c3e06283c68c100ba5f
Devender0023 <kumar.devender0023@gmail.com>
2022-07-11 12:56:57 +1000

    Made a function to return string of factors of a given number using list comprehension.

--- Part_2.py
+++ Part_2.py
@@ -3,4 +3,10 @@ def titleize(sentence):
     words = [word.replace(word[0], word[0].upper()) for word in words]
     return ' '.join(words)
 
-print(titleize("oNLy cAPITALIZe fIRSt"))
+# print(titleize("oNLy cAPITALIZe fIRSt"))
+
+def find_factors(number):
+    factors = [num for num in range(1,number+1) if number%num==0]
+    return factors
+
+print(find_factors(412146))
