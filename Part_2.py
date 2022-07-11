047bff682a98364b7d20e9531bf6da1cebbfa457
Devender0023 <kumar.devender0023@gmail.com>
2022-07-12 01:06:58 +1000

    Added some more practise problems.

--- Part_2.py
+++ Part_2.py
@@ -9,4 +9,34 @@ def find_factors(number):
     factors = [num for num in range(1,number+1) if number%num==0]
     return factors
 
-print(find_factors(412146))
+# print(find_factors(412146))
+
+def includes(collection, element, index=0):
+    if type(collection) in (str, list):
+        new_coll = collection[index:len(collection)]
+        if element in new_coll:
+            return True
+        return False
+    else:
+        if element in collection.values():
+            return True
+        return False
+
+
+# print(includes('abcd','b',1))
+
+def repeat(my_str, num):
+    return my_str*num
+
+# print(repeat('abc', 0))
+
+def truncate(sentence, num):
+    if num < 3:
+        return "Truncation must be at least 3 characters."
+    else:
+        if num-3 > len(sentence):
+            return f'{sentence[:num-3]}'
+        return f'{sentence[:num-3]}...'
+
+
+print(truncate("Problem solving is the best!", 10))
