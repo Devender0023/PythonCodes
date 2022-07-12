25874ce9003bdb17a3a23808caa16cf5fba28ad1
Devender0023 <kumar.devender0023@gmail.com>
2022-07-13 01:15:20 +1000

    Made a function to add numbers of a list in a given range.

--- Part_3.py
+++ Part_3.py
@@ -8,4 +8,18 @@ def two_list_dictionary(lst1, lst2):
 
 
 
-print(two_list_dictionary(['x', 'y', 'z'], [1,2]))
+# print(two_list_dictionary(['x', 'y', 'z'], [1,2]))
+
+
+def range_in_list(lst, start=0, end=0):
+    def add(start, end):
+        sum = 0
+        for i in range(start, end):
+            sum += lst[i]
+        return sum
+    if end == 0 or end > len(lst):
+        return add(start, len(lst))
+    return add(start, end+1)
+
+
+print(range_in_list([], 0, 1))
