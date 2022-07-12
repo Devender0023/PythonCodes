3e8aa1c0746d20c48da2d85a3c6cfb36d0705a73
Devender0023 <kumar.devender0023@gmail.com>
2022-07-13 01:41:57 +1000

    Function to check if the frequency of numbers in an integer is same or not.

--- Part_3.py
+++ Part_3.py
@@ -22,4 +22,38 @@ def range_in_list(lst, start=0, end=0):
     return add(start, end+1)
 
 
-print(range_in_list([], 0, 1))
+# print(range_in_list([], 0, 1))
+
+
+def same_frequency(num1, num2):
+
+    def make_dict(nums):
+        my_dict = {}
+        for num in str(nums):
+            if num not in my_dict.keys():
+                my_dict[num] = 1
+            else:
+                my_dict[num] += 1
+        return my_dict
+
+    dict1 = make_dict(num1)
+    dict2 = make_dict(num2)
+    count = 0
+    for key, value in dict1.items():
+        if key in dict2.keys() and dict1[key] == dict2[key]:
+            count+=1
+    if count == len(dict1):
+        return True
+    return False
+
+
+print(same_frequency(1212, 2211))
+
+
+
+
+
+
+
+
+
