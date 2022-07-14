42e8e8daefd106d990ac288c09a30502453fad78
Devender0023 <kumar.devender0023@gmail.com>
2022-07-14 14:00:43 +1000

    made a function to find duplicate in list.

--- Part_3.py
+++ Part_3.py
@@ -12,14 +12,9 @@ def two_list_dictionary(lst1, lst2):
 
 
 def range_in_list(lst, start=0, end=0):
-    def add(start, end):
-        sum = 0
-        for i in range(start, end):
-            sum += lst[i]
-        return sum
     if end == 0 or end > len(lst):
-        return add(start, len(lst))
-    return add(start, end+1)
+        return sum(lst[start:len(lst)])
+    return sum(lst[start:end+1])
 
 
 # print(range_in_list([], 0, 1))
@@ -27,17 +22,17 @@ def range_in_list(lst, start=0, end=0):
 
 def same_frequency(num1, num2):
 
-    def make_dict(nums):
-        my_dict = {}
-        for num in str(nums):
-            if num not in my_dict.keys():
-                my_dict[num] = 1
-            else:
-                my_dict[num] += 1
-        return my_dict
+    # def make_dict(nums):
+    #     my_dict = {}
+    #     for num in str(nums):
+    #         if num not in my_dict.keys():
+    #             my_dict[num] = 1
+    #         else:
+    #             my_dict[num] += 1
+    #     return my_dict
 
-    dict1 = make_dict(num1)
-    dict2 = make_dict(num2)
+    dict1 = {letter:str(num1).count(letter) for letter in str(num1)}
+    dict2 = {letter:str(num2).count(letter) for letter in str(num2)}
     count = 0
     for key, value in dict1.items():
         if key in dict2.keys() and dict1[key] == dict2[key]:
@@ -55,11 +50,19 @@ def nth(lst, index):
     return lst[index]
 
 
-print(nth(['a','b','c','d'],3))
+# print(nth(['a','b','c','d'],3))
 
 
+def find_the_duplicate(lst):
+    new_list = []
+    for ele in lst:
+        if ele not in new_list:
+            new_list.append(ele)
+        else:
+            return ele
 
 
+print(find_the_duplicate([2,1,3,4]))
 
 
 
